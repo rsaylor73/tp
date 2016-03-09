373e4d9f58d9f7dfbeab8f292b5c03170c31d201
root <root@mx.customphpdesign.com>
2016-03-08 21:33:29 -0500

    1

--- test5.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "include/settings.php";
-if ($_SERVER['HTTP_HOST'] != $GLOBAL['domain']) {
-	$sub = explode(".",$_SERVER['HTTP_HOST']);
-	$redirect = "http://".$GLOBAL['domain']."/".$sub[0];
-	header("Location: $redirect");
-}
-?>
