ff88497cf1f48db9d9f24013a9c03435e4377d51
Robert Saylor <robert@customphpdesign.com>
2016-03-29 19:11:16 -0400

    mobile development

--- view_desktop_upbeat.php
+++ view_desktop_upbeat.php
@@ -123,7 +123,7 @@ $cover = "uploads/$row[userID]/cover/$row[id]/$row[cover_image]";
                                                 if ($row[$var] != "") {
                                                         print "
                                                         
-                                                        <img class=\"img-responsive\" alt=\"slide show\" src=\"uploads/$row[userID]/slide/$row[id]/$row[$var]\" />
+                                                        <img class=\"img-responsive\" alt=\"slide show\" src=\"uploads/$row[userID]/slide/$row[id]/$row[$var]\" width=50% />
                                                         
                                                         ";
                                                 }
